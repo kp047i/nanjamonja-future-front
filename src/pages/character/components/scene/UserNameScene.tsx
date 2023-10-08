c11cb3f612e9f6082e75a16eb36f04b730365aad
SonedaYusuke <yusuke.soneda@gmail.com>
2023-10-08 14:22:12 +0900

    大方完了

--- src/pages/character/components/scene/UserNameScene.tsx
+++ src/pages/character/components/scene/UserNameScene.tsx
@@ -1,13 +1,14 @@
 interface Props {
   next: () => void;
+  setUserName: (name: string) => void;
 }
 
-export const UserNameScene = ({next}: Props) => {
+export const UserNameScene = ({next, setUserName}: Props) => {
 
   return (
     <div>
       <p>名前を入力してね！</p>
-      <input type="text" />
+      <input onChange={e => setUserName(e.target.value)} type="text" />
       <button onClick={next}>決定</button>
     </div>
   )
--- src/pages/character/index.tsx
+++ src/pages/character/index.tsx
@@ -8,18 +8,29 @@ import { useState } from "react";
 
 type Scene = 'username' | 'oekaki' | 'finish';
 
-export const Character = () => {
-  const [scene, setScene] = useState<Scene>('username')
-  return (
-    <CharacterLayout>
-      {scene === 'username' && <UserNameScene next={() => setScene('oekaki')} />}
-      {scene === 'oekaki' && <OekakiScene next={() => setScene('finish')} />}
-      {scene === 'finish' && <FinishScene />}
-    </CharacterLayout>
-  );
-};
-
 const CharacterLayout = styled.div`
   width: 100%;
   height: 100svh;
 `;
+
+export const Character = () => {
+  const [scene, setScene] = useState<Scene>('username');
+  const [userName, setUserName] = useState<string>('');
+
+  const goOekaki = () => {
+    if (userName === '') return;
+    setScene('oekaki');
+  }
+
+  const goFinish = () => {
+    setScene('finish');
+  }
+
+  return (
+    <CharacterLayout>
+      {scene === 'username' && <UserNameScene next={goOekaki} setUserName={setUserName} />}
+      {scene === 'oekaki' && <OekakiScene next={goFinish} />}
+      {scene === 'finish' && <FinishScene />}
+    </CharacterLayout>
+  );
+};
