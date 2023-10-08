d768228df572ea0a8a7a5c5f9066ac65b6db9d3d
Takayuki Miura <takayuki.miura28@gmail.com>
2023-10-08 11:53:03 +0900

    feat: add react router

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,22 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Game } from "./pages/game/index.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/game",
+    element: <Game />,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
